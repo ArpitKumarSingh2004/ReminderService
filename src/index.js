0612062a3e2a097c5a1ebb301cfeeee03acd9338
Arpit Kumar <arpitkumar1237@gmail.com>
2025-05-27 10:42:23 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ const { PORT } = require('./config/serverConfig');
 
 const TicketController = require('./controllers/ticket-controller');
 const EmailService = require('./services/email-service');
-const { sendBasicEmail } = require('./services/email-service');
-
 
 const jobs = require('./utils/job');
-// const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
 const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
@@ -19,19 +17,12 @@ const setupAndStartServer = async () => {
 
     app.post('/api/v1/tickets', TicketController.create);
 
-    // const channel = await createChannel();
-    // subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
         // jobs();
-
-        sendBasicEmail(
-            'arpit@admin.com',
-            'arpitt.testing@gmail.com',
-            'THIS IS A TESTING Email',
-            'Hey, this is a test email from the Reminder Service. Please ignore it.'
-        )
         
     });
 }
